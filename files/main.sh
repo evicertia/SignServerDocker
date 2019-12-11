31141d7f6696a7629324bb1d5b6f01b8739b2a5c
Pablo Ruiz <pablo.ruiz@gmail.com>
2019-12-11 03:47:57 +0100

    Added variable to main.sh in order to control pkcs#11's KeepAlive setting.

--- files/main.sh
+++ files/main.sh
@@ -31,6 +31,7 @@ setup_pkcs11 () {
 	[ "${CRYPTOSERVER:-}" != "" ] || die 2 "Missing CRYPTOSERVER env variable."
 	sed -i'' -e "s/CRYPTOSERVER/${CRYPTOSERVER}/" /opt/utimaco/p11/libcs_pkcs11_R2.cfg
 	sed -i'' -e "s/CS_PKCS11_LOGLEVEL/${CS_PKCS11_LOGLEVEL:-3}/" /opt/utimaco/p11/libcs_pkcs11_R2.cfg
+	sed -i'' -e "s/^KeepAlive = /${CS_PKCS11_KEEPALIVE:-true}/" /opt/utimaco/p11/libcs_pkcs11_R2.cfg
 	export CS_PKCS11_R2_CFG=/opt/utimaco/p11/libcs_pkcs11_R2.cfg
 }
 
